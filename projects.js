36e7ae7cb1bf0c19e56286fe07a7aea2abe975e6
emranazizi-web <emranwebsite7@gmail.com>
2026-04-24 00:51:17 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -1,1 +1,21 @@
+fetch("projects-data.json")
+    .then(response => response.json())
+    .then(data => {
 
+        let container = document.getElementById("projects-container");
+
+        data.forEach(project => {
+
+            let div = document.createElement("div");
+            div.classList.add("card");
+
+            div.innerHTML = `
+                <h3>${project.title}</h3>
+                <p>${project.description}</p>
+                <small>${project.tech}</small>
+            `;
+
+            container.appendChild(div);
+        });
+
+    });
