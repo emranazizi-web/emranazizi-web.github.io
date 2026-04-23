ef1b8bd5240ebc677979cbf92c6acaa76d999db4
emranazizi-web <emranwebsite7@gmail.com>
2026-04-24 00:41:28 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -9,3 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 100);
 
 });
+
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+});
