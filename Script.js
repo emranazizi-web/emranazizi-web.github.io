1604773adbed5a431665363e4166cfc151c9488e
emranazizi-web <emranwebsite7@gmail.com>
2026-04-24 00:45:43 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -18,3 +18,19 @@ document.addEventListener("mousemove", (e) => {
     cursor.style.left = e.pageX + "px";
     cursor.style.top = e.pageY + "px";
 });
+
+function revealOnScroll() {
+    let elements = document.querySelectorAll(".reveal");
+
+    elements.forEach(el => {
+        let windowHeight = window.innerHeight;
+        let elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("active");
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
