44b10437ec8399c2e0aa3a1075b78a9aa263c5d8
emranazizi-web <emranwebsite7@gmail.com>
2026-04-24 13:15:38 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -73,3 +73,13 @@ function openImg(img) {
 function closeImg() {
     document.getElementById("lightbox").style.display = "none";
 }
+
+function openImg(img) {
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeImg() {
+    document.getElementById("lightbox").style.display = "none";
+}
+
